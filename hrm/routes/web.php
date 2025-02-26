a540b8adce28d85ad8f75a1a9acfe89a6f74563a
YvenzKlyd <160690722+YvenzKlyd@users.noreply.github.com>
2025-02-27 05:03:51 +0800

    User and Admin
    Authentication

--- hrm/routes/web.php
+++ hrm/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,9 +9,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -18,3 +17,17 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// user routes
+Route::middleware(['auth','userMiddleware'])->group(function(){
+
+    Route::get('dashboard', [UserController::class,'index'])->name('dashboard');
+
+});
+
+// admin routes
+Route::middleware(['auth','adminMiddleware'])->group(function(){
+
+    Route::get('/admin/dashboard', [AdminController::class,'index'])->name('admin.dashboard');
+
+});
